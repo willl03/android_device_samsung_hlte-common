55031c580eddae9ac19c985662f9dac8e9251554
Janson Kang <temasek71@gmail.com>
2015-02-12 16:37:28 +0800

    hlte: Enable tethering without provisioning app
    Fixes wifi tethering on tmobile

--- system_prop.mk
+++ system_prop.mk
@@ -47,6 +47,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sensors=1
 
+# Tethering
++PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true
+
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
